2ed8aee473bf4c913b99051fa921f96301c45733
Lajos Santa <santa.lajos@coldline.hu>
2016-02-07 11:47:33 +0100

    Wrong dependency fixed

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name = "PCA9685-driver-http",
     description = "Web interface for PCA9685 pwm (LED/Servo) controller",
-    version = "1.0.0",
+    version = "1.0.1",
     author = 'Lajos Santa',
     author_email = 'santa.lajos@coldline.hu',
     url = 'https://github.com/voidpp/PCA9685-driver-http.git',
     install_requires = [
         "Flask==0.10.1",
-        "PCA9685-driver==1.0.0",
+        "PCA9685-driver==1.1.0",
         "voidpp-tools==1.5.0",
         "querystring-parser==1.2.3",
         "six==1.10.0",
